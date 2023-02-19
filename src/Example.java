3a8b5e999d85f4eb14997b0943379e20cebfc787
KDissanayke-Bootcamp <ssndkavindra@gmail.com>
2023-02-19 09:54:20 +0530

    Initial commit 01

---
+++ src/Example.java
@@ -0,0 +1,19 @@
+class Example{
+    public static boolean isPalindrome(int num){
+        int reverse=0;
+        int temp=num;
+        while(num!=0){
+            reverse=reverse*10;
+            reverse=reverse+num%10;
+            num/=10;
+        }
+        return reverse==temp?true:false;
+    }
+    public static void main(String args[]){
+        for(int i=0; i<1000; i++){
+            if(isPalindrome(i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
