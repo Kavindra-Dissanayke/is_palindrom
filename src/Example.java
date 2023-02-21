b0791e19a86a57c414f811d2740f80b5c34b8208
KDissanayke-Bootcamp <ssndkavindra@gmail.com>
2023-02-21 11:11:41 +0530

    update 06

--- src/Example.java
+++ src/Example.java
@@ -18,6 +18,7 @@ class Example{
     }
 }
 */
+/*
 
 import java.util.*;
 class Example{
@@ -51,3 +52,70 @@ class Example{
         System.out.println("Total : "+total);
     }
 }
+*/
+
+/*
+import java.text.DecimalFormat;
+import java.util.*;
+
+class Example {
+
+    static final DecimalFormat df = new DecimalFormat("0.00");//0.000 , 0.0000
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input array size : ");
+        int num = input.nextInt();
+        Random random = new Random();
+        int[] a = new int[num];
+
+        //Read,input, store integers into array “a”  Using Random numbers  [0 to 100]
+        for (int i = 0; i < num; i++) {
+            a[i] = random.nextInt(100);
+        }
+
+        //--------------------------------
+        int total = 0;
+        for (int i = 0; i < num; i++) {
+
+            total += a[i];
+        }
+      */
+/*  int max = 0;
+        for (int i = 0; i < num; i++) {
+            if (a[i] > max) {
+                max = a[i];
+
+            }
+        }
+*//*
+
+        int max =a[0];
+        for (int i = 1; i < num; i++) {
+            if (a[i] > max) {
+                max = a[i];
+
+            }
+        }
+
+        int min = a[0];
+        for (int i = 1; i < num; i++) {
+            if (a[i] < min) {
+                min = a[i];
+
+            }
+        }
+        double avg=(double)total/num;
+        //double avg=total/num;
+        System.out.print("[");
+        for (int i = 0; i < num; i++) {
+            System.out.print(a[i] + ", ");
+        }
+        System.out.println("\b\b]");
+
+        System.out.println("Total : " + total);
+        System.out.println("Max : " + max);
+        System.out.println("Min : " + min);
+        System.out.println("Average : "+df.format(avg));
+    }
+}
+*/
